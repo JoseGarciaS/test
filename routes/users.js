16db3cb414b4c30dfeee32bea3238dbfc4b8b08d
Jose Garcia <jargarcias@unitec.edu>
2024-01-29 18:34:29 -0600

    adding weather api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var dotenv = require('dotenv').config()
 
-/* GET users listing. */
+var axios = require('axios')
+
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});*/
+
+router.get('/', function (req, res, next) {
+  console.log(process.env.WEATHER_API)
+  res.send(process.env.WEATHER_API)
 });
+
+router.get('/weather', function (req, res, next) {
+ 
+  axios({
+    method: 'get',
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${process.env.WEATHER_API}`
+  })
+    .then(function (response) {
+      res.send(response.data)
+  });
+  
+  
+  
+});
+
 
 module.exports = router;
